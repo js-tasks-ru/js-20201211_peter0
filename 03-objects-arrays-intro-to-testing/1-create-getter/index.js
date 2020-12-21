d24389c1c67aec5f0d9a22de4b1438e50a214b35
PMorozov <petia.morozov@gmail.com>
2020-12-21 23:11:36 +0100

    3.1 add createGetter implementation

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function (obj) {
+    let result = obj;
+    for (const key of path.split('.')) {
+      if (result) {
+        result = result[key];
+      }
+    }
+    return result;
+  };
 }
