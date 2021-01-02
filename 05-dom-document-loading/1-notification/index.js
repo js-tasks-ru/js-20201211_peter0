4e7fdee72d404cb2c71fff1c1449c8f5aebf89d0
PMorozov <petia.morozov@gmail.com>
2021-01-02 13:18:23 +0100

    5.1. add NotificationMessage component implementation

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
 
+  static activeNotification;
+
+  constructor(message, {duration = 1000, type = 'success'} = {}) {
+    NotificationMessage.removeActiveNotification();
+
+    this.message = message;
+    this.duration = duration;
+    this.durationSec = duration / 1000;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationSec}s">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+            <div class="notification-header">success</div>
+            <div class="notification-body">
+              ${this.message}
+            </div>
+          </div>
+        </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+    NotificationMessage.activeNotification = this.element;
+  }
+
+  show(element = document.body) {
+    element.append(this.element);
+
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  static removeActiveNotification() {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    NotificationMessage.activeNotification = null;
+  }
+
 }
